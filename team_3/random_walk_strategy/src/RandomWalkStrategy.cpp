fc4af335935b3ad1281d06e49bc1643697e1e04e
Leonhard Kuboschek <x32000@gmail.com>
2016-03-07 13:36:31 +0100

    Remove extra condition

--- team_3/random_walk_strategy/src/RandomWalkStrategy.cpp
+++ team_3/random_walk_strategy/src/RandomWalkStrategy.cpp
@@ -7,49 +7,48 @@ bool RandomWalkStrategy::getCircleVisible() { return circleVisible; }
 
 void RandomWalkStrategy::receiveCirclePosition(
     const geometry_msgs::Pose2D::ConstPtr &circlePose) {
-  if ((circlePose->x - -1.0) < EPSILON) {
-    if (circlePose->x == -1) {
-      circleVisible = false;
-      return;
-    }
-
-    circleVisible = true;
-    circleAngle = circlePose->theta * (180 / M_PI);
-    circleDistance = sqrt(pow(circlePose->x, 2) + pow(circlePose->y, 2));
-
-    // ROS_INFO("Circle angle: %f", circleAngle);
-    // ROS_INFO("Circle distance: %f", circleDistance);
+  if (circlePose->x == -1) {
+    circleVisible = false;
+    return;
   }
 
-  void RandomWalkStrategy::receiveLaserScan(
-      const sensor_msgs::LaserScan::ConstPtr &laserScan) {
-    lastScan = *laserScan;
-  }
+  circleVisible = true;
+  circleAngle = circlePose->theta * (180 / M_PI);
+  circleDistance = sqrt(pow(circlePose->x, 2) + pow(circlePose->y, 2));
 
-  const geometry_msgs::Twist RandomWalkStrategy::getControlOutput() {
-    geometry_msgs::Twist msg;
+  // ROS_INFO("Circle angle: %f", circleAngle);
+  // ROS_INFO("Circle distance: %f", circleDistance);
+}
 
-    if (circleVisible) {
-      float variation = SCAN_CENTER - circleAngle;
+void RandomWalkStrategy::receiveLaserScan(
+    const sensor_msgs::LaserScan::ConstPtr &laserScan) {
+  lastScan = *laserScan;
+}
 
-      if (abs(variation) > VARIATION_THRESHOLD)
-        correcting = true;
+const geometry_msgs::Twist RandomWalkStrategy::getControlOutput() {
+  geometry_msgs::Twist msg;
 
-      if (abs(variation) < VARIATION_THRESHOLD - HYSTERESIS)
-        correcting = false;
+  if (circleVisible) {
+    float variation = SCAN_CENTER - circleAngle;
 
-      // ROS_INFO("V: %f\tC: %s", variation, correcting ? "true" : "false");
+    if (abs(variation) > VARIATION_THRESHOLD)
+      correcting = true;
 
-      if (correcting) {
-        msg.angular.z = std::min(MAX_TURN, TURN_CORRECTION * variation);
-        msg.linear.x = LINEAR_VEL / std::min(4.0, abs(variation) / 10.0);
-      } else {
-        msg.linear.x = LINEAR_VEL;
-      }
+    if (abs(variation) < VARIATION_THRESHOLD - HYSTERESIS)
+      correcting = false;
+
+    // ROS_INFO("V: %f\tC: %s", variation, correcting ? "true" : "false");
+
+    if (correcting) {
+      msg.angular.z = std::min(MAX_TURN, TURN_CORRECTION * variation);
+      msg.linear.x = LINEAR_VEL / std::min(4.0, abs(variation) / 10.0);
     } else {
-      msg.linear.x = 0;
-      msg.angular.z = SCAN_VELOCITY;
+      msg.linear.x = LINEAR_VEL;
     }
-
-    return msg;
+  } else {
+    msg.linear.x = 0;
+    msg.angular.z = SCAN_VELOCITY;
   }
+
+  return msg;
+}
