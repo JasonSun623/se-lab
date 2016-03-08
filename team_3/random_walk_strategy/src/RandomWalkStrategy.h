edac26c3054964a2cc31e0ef4da3264f367eb66a
Felix Schmoll <eggi.innovations@gmail.com>
2016-03-08 14:22:07 +0000

    Comment RandomWalkStrategy

--- team_3/random_walk_strategy/src/RandomWalkStrategy.h
+++ team_3/random_walk_strategy/src/RandomWalkStrategy.h
@@ -1,57 +1,111 @@
+/** @file RandomWalkStrategy.h
+  * @brief Receives data for wall-distance and suggested half-circle-position
+  *and computes which move to make next.
+  *
+  * If a half-circle is detected it moves towards it and otherwise it moves
+  *forward until it comes to close to an object and turns. It stops after
+  *hitting a half-circle.
+  *
+  * @author Leonhard Kuboschek
+  */
 #ifndef RANDOMWALKSTRATEGY_H
 #define RANDOMWALKSTRATEGY_H
 
+/* -- Includes -- */
+
+/* ROS include */
 #include <ros/ros.h>
+
+/* ROS message type includes */
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose2D.h>
+
+/* libc includes */
 #include <cmath>
 
+/**
+  * @brief Speed
+  */
 #define LINEAR_VEL 1.0
 
-// The angle of circle detector that is straight ahead [deg]
+/**
+ * @brief The angle of circle detector that is straight ahead [deg]
+ */
 #define SCAN_CENTER 90
 
-// The amount of variation that is allowed before direction is corrected [deg]
+/**
+ * @brief The amount of variation that is allowed before direction is corrected
+ * [deg]
+ */
 #define VARIATION_THRESHOLD 8
+
+/**
+  * @brief The value for which realignment is being done. If the value is
+ * smaller we don't correct anymore. [deg]
+  */
 #define HYSTERESIS 2
 
-// Angular velocity value that is used to turn
+/**
+ * @brief Angular velocity value that is used to turn
+ */
 #define TURN_CORRECTION 0.05
+
+/**
+  * @brief Upper speed for turning rate
+  */
 #define MAX_TURN 0.8
 
-// Angular velocity for scanning for circle
+/**
+ * @brief Angular velocity for scanning for circle
+ */
 #define SCAN_VELOCITY 0.5
 
-// Minimum allowed distance to obstacles
+/**
+ * @brief Minimum allowed distance to obstacles
+ */
 #define MIN_DISTANCE 0.5
 
-/** Implements the random walk strategy.
-  * If a half-circle is detected it moves towards it, otherwise it just turns at
-  * the same spot.
+/**
+ * @brief Number of laser samples
+ */
+#define RANGES 250
+
+/**
+ * @brief Deviation for comparing floating point numbers
+ */
+#define EPSILON 0.05
+
+/**
+  * @brief Strategy for Random Walk
   */
 class RandomWalkStrategy {
 public:
   /**
-    * Receives a message with a halfcircle-pose or a dummy message.
-    * Currently just uses the angle for actual navigation.
+    * @brief Receives a message with a halfcircle-pose or a dummy message and
+   * sets the corresponding class-internal values.
     * @param circlePose A message with a supposed circle or x == -1 for a dummy.
+    * @return No return value.
     */
   void receiveCirclePosition(const geometry_msgs::Pose2D::ConstPtr &circlePose);
 
   /**
-    * Receives a LaserScan in order to avoid walls.
+    * @brief Setter for LaserScans.
     * @param laserScan LaserScan with distances to walls.
+    * @return No return value.
     */
   void receiveLaserScan(const sensor_msgs::LaserScan::ConstPtr &laserScan);
 
   /**
-    * Cointains the actual logic of the strategy.
+    * @brief Contains the actual logic of the strategy.
     * Decides on where to move next and by how much.
     * @return Next move to be done.
     */
   const geometry_msgs::Twist getControlOutput();
 
+  /*
+   * @brief Getter returning if half-circle is visible.
+   */
   bool getCircleVisible();
 
 private:
@@ -62,17 +116,18 @@ private:
   bool correcting;
 
   /**
-    * Last received LaserScan.
+    * @brief Last received LaserScan.
     */
   sensor_msgs::LaserScan lastScan;
 
   /**
-    * Find the smallest laser scan reading
+    * @brief Find the smallest laser scan reading
+    * @return Minimum distance to an object.
     */
   float findMinim(int num_readings);
 
   /**
-    * Different states within the strategy.
+    * @brief Different states within the strategy.
     */
   enum State { STARTUP, LOOKING, FOUND };
 
