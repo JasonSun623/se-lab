bef946eb00585c8658ed3aa81eecf90eeab4ec4c
Felix Schmoll <eggi.innovations@gmail.com>
2016-03-22 17:28:22 +0000

    Add test case to halfCircleDetection

--- team_3/half_circle_detection/src/test/HalfCircleDetectionTest.cpp
+++ team_3/half_circle_detection/src/test/HalfCircleDetectionTest.cpp
@@ -14,7 +14,7 @@
 #include <ros/package.h>
 
 /* Include header for class to be tested */
-#include "../include/HalfCircleDetector.h"
+#include "../../include/HalfCircleDetector.h"
 
 /** @brief This test case checks on a very basic example if the detector detects
  * an actual semicircle to a certain accuracy. */
@@ -22,7 +22,7 @@ TEST(HalfCircleDetectionTestSuite, circleDetectionTestCase) {
   HalfCircleDetector *h = new HalfCircleDetector();
 
   std::string path = ros::package::getPath("half_circle_detection");
-  path += "/test/laserScan.bag";
+  path += "/src/test/laserScan_HalfCircle.bag";
 
   rosbag::Bag bag;
   bag.open(path, rosbag::bagmode::Read);
@@ -44,6 +44,40 @@ TEST(HalfCircleDetectionTestSuite, circleDetectionTestCase) {
       ASSERT_NEAR(pose.x, 0, 5);
       ASSERT_NEAR(pose.y, 2, 5);
       ASSERT_NEAR(pose.theta, 1.6, 0.2);
+    }
+  }
+
+  delete h;
+}
+
+/** @brief This test case checks if the detector is giving false positives, thus
+ * signaling a halfCircle when there is no halfCircle.*/
+TEST(HalfCircleDetectionTestSuite, falsePositiveTestCase) {
+  HalfCircleDetector *h = new HalfCircleDetector();
+
+  std::string path = ros::package::getPath("half_circle_detection");
+  path += "/src/test/laserScan_noHalfCircle.bag";
+
+  rosbag::Bag bag;
+  bag.open(path, rosbag::bagmode::Read);
+
+  std::vector<std::string> topics = {"base_scan"};
+
+  rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+  for (rosbag::MessageInstance m : view) {
+
+    sensor_msgs::LaserScan::ConstPtr ptr =
+        m.instantiate<sensor_msgs::LaserScan>();
+    if (ptr != NULL) {
+
+      h->receiveLaserScan(ptr);
+
+      geometry_msgs::Pose2D pose = h->getHalfCirclePose();
+
+      ASSERT_NEAR(pose.x, 0, 0.01);
+      ASSERT_NEAR(pose.y, 0, 0.01);
+      ASSERT_NEAR(pose.theta, 0, 0.01);
     }
   }
 
