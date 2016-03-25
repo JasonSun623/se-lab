a1c6153b6beff7223c3171eb77840220772ec893
mgladkova <gladkovamaria14@gmail.com>
2016-03-25 11:19:23 +0100

    Optimize existing code

--- team_3/line_detection/src/line_detectionV2.cpp
+++ team_3/line_detection/src/line_detectionV2.cpp
@@ -45,7 +45,7 @@ cv::Vec4i getAverSlope(std::vector<std::pair<cv::Vec4i, float>> vec) {
 int getDifference(int a, int b) { return abs(a - b); }
 
 int main() {
-  cv::Mat src = cv::imread("Image.jpg");
+  cv::Mat src = cv::imread("Image2.jpg");
   if (!src.data)
     return -1;
 
@@ -95,18 +95,7 @@ int main() {
       //     res.push_back(getAverSlope(temp));
       //     temp.clear();
       //     std::cout << "3" << std::endl;
-      //     for (int j = 0; j < res.size(); j++){
-      //         if (abs(res[j][1] - lines1[i][1]) < 3){
-      //             pushed = false;
-      //             break;
-      //         }
-      //     }
 
-      //     if (pushed){
-      //         std::pair<cv::Vec4i, float> p =
-      //         std::make_pair(lines1[i],slope);
-      //         temp.push_back(p);
-      //     }
     } else if (slope > 0 && fabs(temp[0].second - slope) < 0.3 &&
                abs(temp[temp.size() - 1].first[0] - lines1[i][0]) <
                    getDifference(temp[temp.size() - 1].first[0],
@@ -127,25 +116,46 @@ int main() {
       std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
       std::cout << "4 ?? " << fabs(temp[0].second + slope) << std::endl;
-    } else {
+    } else if (abs(temp[temp.size() - 1].first[2] - lines1[i][0]) < 2 &&
+               abs(temp[temp.size() - 1].first[3] - lines1[i][1]) < 2) {
       // if (abs(temp[temp.size() - 1].second - slope) > 0.3 ||
       // abs(temp[temp.size() - 1].first[0] - lines1[i][0]) >
       // getDifference(temp[temp.size() - 1].first[0], temp[temp.size() -
       // 1].first[2]) || abs(temp[temp.size() - 1].first[1] - lines1[i][1]) <
       // getDifference(temp[temp.size() - 1].first[1], temp[temp.size() -
       // 1].first[3])){
-      res.push_back(getAverSlope(temp));
-      temp.clear();
-      std::cout << "5" << std::endl;
-
       std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
+      std::cout << "5 ?? " << fabs(temp[0].second + slope) << std::endl;
+    } else {
+      res.push_back(getAverSlope(temp));
+      temp.clear();
+      std::cout << "6" << std::endl;
+
+      for (int j = 0; j < res.size(); j++) {
+        if (slope > 0 && fabs(calcSlope(res[j]) - slope) < 2 ||
+            slope < 0 && fabs(calcSlope(res[j]) + slope) < 2) {
+          std::cout << "HH" << std::endl;
+          pushed = false;
+          break;
+        }
+      }
+
+      if (pushed) {
+        std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
+        std::cout << "YES" << std::endl;
+        temp.push_back(p);
+        pushed = true;
+      }
+
+      // std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i],slope);
+      // temp.push_back(p);
     }
 
     if (!temp.empty() && i == lines1.size() - 1) {
       res.push_back(getAverSlope(temp));
       temp.clear();
-      std::cout << "6" << std::endl;
+      std::cout << "7" << std::endl;
     }
 
     std::cout << "LINES " << lines1[i][0] << " , " << lines1[i][1] << " , "
@@ -163,14 +173,13 @@ int main() {
   for (size_t i = 0; i < lines1.size(); i++) {
     line(color_dst1, cv::Point(lines1[i][0], lines1[i][1]),
          cv::Point(lines1[i][2], lines1[i][3]), cv::Scalar(0, 0, 255), 3, 8);
-    // std::cout << "lines1 size " << lines1.size() << std::endl;
   }
 
   // std::cout << lines1.size() << std::endl;
-
+  std::cout << "lines1 size " << lines1.size() << std::endl;
   std::cout << res.size() << " lines detected " << std::endl;
 
-  cv::imshow("src", src);
+  // cv::imshow("src", src);
   cv::imshow("dst1", color_dst1);
   cv::imshow("dst2", color_dst2);
   cv::waitKey();
