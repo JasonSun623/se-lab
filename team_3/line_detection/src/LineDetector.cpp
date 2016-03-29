1905c239fc4c6174fec43b46aaf6e078b247615a
mgladkova <gladkovamaria14@gmail.com>
2016-03-29 22:33:28 +0200

    Document Line Detection #29

--- team_3/line_detection/src/LineDetector.cpp
+++ team_3/line_detection/src/LineDetector.cpp
@@ -1,11 +1,9 @@
-#include "LineDetector.h"
-
-/**
-  * @brief Limiting integers to be within a certain range.
+/** @file LineDetector.cpp
+  * Implementation of LineDetector.h
+  * @author Mariia Gladkova
   */
-#define RANGE(l, x, r) (std::max((l), std::min((r), (x))))
 
-#define STRETCH_FACTOR 100
+#include "../include/LineDetector.h"
 
 void LineDetector::receiveLaserScan(
     const sensor_msgs::LaserScan::ConstPtr &laserScan) {
@@ -14,44 +12,40 @@ void LineDetector::receiveLaserScan(
 }
 
 float LineDetector::calcSlope(cv::Vec4i one) {
+  // by formula m = (y-y0)/(x-x0)
   return ((one[3] - one[1]) * 1.0 / (one[2] - one[0]));
 }
 
 cv::Vec4i
-LineDetector::getAverSlope(std::vector<std::pair<cv::Vec4i, float>> vec) {
-  // std::sort(vec.begin(), vec.end(), compareSlope);
-  // int idx = vec.size()/2;
+LineDetector::getAverLine(std::vector< std::pair< cv::Vec4i, float > > vec) {
   cv::Vec4i average;
+  // initial vector is sorted by x position of the starting point
+  // thus the leftmost is taken
   average[0] = vec[0].first[0];
   average[1] = vec[0].first[1];
 
+  // sort by x coordinate of the end point and take first element
+  // endpoint position
   std::sort(vec.begin(), vec.end(), compareEnd);
 
   average[2] = vec[0].first[2];
   average[3] = vec[0].first[3];
-
-  // std::cout << average[0] << " , " << average[1] << " , " << average[2] << "
-  // , "<< average[3] << std::endl;
 
   return average;
 }
 
 int LineDetector::getDifference(int a, int b) { return abs(a - b); }
 
-void LineDetector::printLinesImage(cv::Mat dst, std::vector<cv::Vec4i> lines) {
+void LineDetector::printLinesImage(cv::Mat dst,
+                                   std::vector< cv::Vec4i > lines) {
   for (size_t i = 0; i < lines.size(); i++) {
     line(dst, cv::Point(lines[i][0], lines[i][1]),
          cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
-    // std::cout << lines[i][0] << " , " << lines[i][1] << " , " << lines[i][2]
-    //<< " , " << lines[i][3] << std::endl;
   }
 }
 
-/** @brief Interpolates the data up to the requested resolution using linear
- * interpolation.
- */
 float LineDetector::interpolate(int index, int resolution,
-                                std::vector<float> data) {
+                                std::vector< float > data) {
   int size = data.size();
   float step = 1.0 / ((float)resolution);
 
@@ -72,11 +66,6 @@ float LineDetector::interpolate(int index, int resolution,
   return value;
 }
 
-/**
- * @brief Converts the laserScan-data from polar into cartesian coordinates.
- * Then cleans the data from various problems and finally translates the points
- * into pixels on an actual image (in form of an OpenCV-matrix).
- */
 cv::Mat LineDetector::createOpenCVImageFromLaserScan(
     const sensor_msgs::LaserScan::ConstPtr &laserScan) {
   LineDetector::points.clear();
@@ -113,95 +102,94 @@ cv::Mat LineDetector::createOpenCVImageFromLaserScan(
 
     LineDetector::points.push_back(std::make_pair(x, y));
 
-    image.at<cv::Vec3b>(cv::Point(x, y)) = cv::Vec3b(200, 200, 200);
+    image.at< cv::Vec3b >(cv::Point(x, y)) = cv::Vec3b(200, 200, 200);
   }
 
-  cv::imwrite(
-      "/home/mgladkova/copy_ws/src/team-3/team_3/line_detection/src/Image.jpg",
-      image);
+  std::string path = ros::package::getPath("wall_following_strategy");
+  path += "/src/Image.jpg";
+  cv::imwrite(path, image);
+  cv::waitKey(20);
 
   return image;
 }
 
-void LineDetector::removeLines(std::vector<cv::Vec4i> lines1) {
-  std::vector<std::pair<cv::Vec4i, float>> temp;
-
+void LineDetector::removeLines(std::vector< cv::Vec4i > lines1) {
+  // "bucket" for each of the group of "similar" lines
+  std::vector< std::pair< cv::Vec4i, float > > temp;
+  // sort the lines by the x coordinate of the starting point
   std::sort(lines1.begin(), lines1.end(), compareStart);
 
   bool pushed = true;
 
   int count = 0;
+  // loop through all the lines that have been detected by HoughLinesP
   for (size_t i = 0; i < lines1.size(); i++) {
     float slope = calcSlope(lines1[i]);
-    // std::cout << slope << std::endl;
-
+    // if the bucket is empty push the current line to be processed
     if (temp.empty()) {
-      std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
+      std::pair< cv::Vec4i, float > p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
-      // std::cout << "1" << std::endl;
-    } else if (slope > 0 && fabs(temp[0].second - slope) < 0.3 &&
+      // case 1 of similar line segments: the slopes are almost the same,
+      // starting position of the x-coordinate of the second line segment
+      // that is being processed lies between starting and ending
+      // points of first line segment. Taking into account that the line
+      // segments
+      // are not too far apart in y-direction (e.g. two  parallel walls)
+
+    } else if (fabs(temp[temp.size() - 1].second - slope) < 0.3 &&
                abs(temp[temp.size() - 1].first[0] - lines1[i][0]) <
                    getDifference(temp[temp.size() - 1].first[0],
                                  temp[temp.size() - 1].first[2]) &&
                compareY(temp[temp.size() - 1].first[1], lines1[i][1]) <
                    compareY(temp[temp.size() - 1].first[1],
                             temp[temp.size() - 1].first[3])) {
-      std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
+      std::pair< cv::Vec4i, float > p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
-      // std::cout << "3 ?? " << fabs(temp[0].second - slope) << std::endl;
-    } else if (slope < 0 && fabs(temp[temp.size() - 1].second - slope) < 0.3 &&
-               abs(temp[temp.size() - 1].first[0] - lines1[i][0]) <
-                   getDifference(temp[temp.size() - 1].first[0],
-                                 temp[temp.size() - 1].first[2]) &&
-               compareY(temp[temp.size() - 1].first[1], lines1[i][1]) <
-                   compareY(temp[temp.size() - 1].first[1],
-                            temp[temp.size() - 1].first[3])) {
-      std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
-      temp.push_back(p);
-      // std::cout << "4 ?? " << fabs(temp[0].second + slope) << std::endl;
+      // case 2 and 3 of similar lines: the slopes are almost the same, but
+      // for the second line segment x coordinate of the starting point lies a
+      // bit farther
+      // from the endpoint of the first line segment
     } else if (slope < 0 && fabs(temp[temp.size() - 1].second - slope) < 0.3 &&
                abs(temp[temp.size() - 1].first[2] - lines1[i][0]) < 2) {
-      std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
+      std::pair< cv::Vec4i, float > p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
-      // std::cout << "5 ?? " << fabs(temp[0].second + slope) << std::endl;
     } else if (slope > 0 &&
                compareY(temp[temp.size() - 1].first[3], lines1[i][1]) < 2 &&
                fabs(temp[temp.size() - 1].second - slope) < 0.3) {
-      std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
+      std::pair< cv::Vec4i, float > p = std::make_pair(lines1[i], slope);
       temp.push_back(p);
     } else {
-      res.push_back(getAverSlope(temp));
+      // all the similar line segments were pushed - find the average of them
+      // and push
+      // in a resulting vector of line segments
+      res.push_back(getAverLine(temp));
       temp.clear();
-      // std::cout << "6" << std::endl;
-
+      // as line segments sorted by x-coordinate of the starting point can be
+      // shuffled
+      // with respect to the wall they belong to we need to check before
+      // processing
+      // a new line segment whether the lines similar to it were processed
+      // before
       for (int j = 0; j < res.size(); j++) {
         if (slope > 0 && fabs(calcSlope(res[j]) - slope) < 2 ||
             slope < 0 && fabs(calcSlope(res[j]) + slope) < 2) {
-          // std::cout << "HH" << std::endl;
           pushed = false;
           break;
         }
       }
 
       if (pushed) {
-        std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i], slope);
-        // std::cout << "YES" << std::endl;
+        std::pair< cv::Vec4i, float > p = std::make_pair(lines1[i], slope);
         temp.push_back(p);
         pushed = true;
       }
-
-      // std::pair<cv::Vec4i, float> p = std::make_pair(lines1[i],slope);
-      // temp.push_back(p);
     }
 
+    // always process the current bucket before the end of vector of line
+    // segments
     if (!temp.empty() && i == lines1.size() - 1) {
-      res.push_back(getAverSlope(temp));
+      res.push_back(getAverLine(temp));
       temp.clear();
-      // std::cout << "7" << std::endl;
     }
-
-    // std::cout << "LINES " << lines1[i][0] << " , " << lines1[i][1] << " , "
-    //<< lines1[i][2] << " , " << lines1[i][3] << std::endl;
-    // std::cout << variationStart << std::endl;
   }
 }
--- team_3/line_detection/src/line_detectionV2.cpp
+++ team_3/line_detection/src/line_detectionV2.cpp
@@ -1,4 +1,9 @@
-#include "LineDetector.h"
+/** @file LineDetector.cpp
+  * Implementation of LineDetector.h
+  * @author Mariia Gladkova
+  */
+
+#include "../include/LineDetector.h"
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
@@ -7,19 +12,19 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "line_publisher");
   ros::NodeHandle n;
 
-  std::unique_ptr<LineDetector> detector(new LineDetector());
+  // smart pointer used
+  std::unique_ptr< LineDetector > detector(new LineDetector());
   ros::Subscriber sub =
       n.subscribe("base_scan", 1, &LineDetector::receiveLaserScan, &*detector);
 
   cv::Mat bw, color_dst;
-  std::vector<cv::Vec4i> lines1;
+  std::vector< cv::Vec4i > lines1;
 
   ros::spinOnce();
   ros::Rate loop_rate(10);
   while (ros::ok()) {
-    // Please specify your absolute directory!
-    cv::Mat src = cv::imread("/home/mgladkova/copy_ws/src/team-3/team_3/"
-                             "line_detection/src/Image.jpg");
+    std::string path = ros::package::getPath("wall_following_strategy/src/");
+    cv::Mat src = cv::imread(path + "Image.jpg");
 
     if (!src.data) {
       return -1;
@@ -27,30 +32,17 @@ int main(int argc, char **argv) {
 
     cv::Canny(src, bw, 50, 200, 3);
     cvtColor(bw, color_dst, CV_GRAY2BGR);
-
     cv::HoughLinesP(bw, lines1, 1, CV_PI / 180, 20, 10, 10);
+    // after applying Hough Transform remove unnecessary lines
     detector->removeLines(lines1);
-    detector->printLinesImage(color_dst, detector->getLines());
-    // Please specify your absolute directory!
-    cv::imwrite("/home/mgladkova/copy_ws/src/team-3/team_3/line_detection/src/"
-                "Processed.jpg",
-                color_dst);
+    cv::imwrite(path + "Processed.jpg", color_dst);
     cv::waitKey(25);
+
     std::cout << detector->getNumLines() << " lines detected " << std::endl;
     lines1.clear();
     detector->clearData();
     ros::spinOnce();
     loop_rate.sleep();
-
-    // detector->printLinesImage(color_dst1, lines1);
-    // detector->printLinesImage(color_dst2, detector->getLines());
-
-    // // std::cout << lines1.size() << std::endl;
-    // std::cout << "lines1 size " << lines1.size() << std::endl;
-
-    // cv::imshow("src", src);
-    // cv::imshow("dst1", color_dst1);
-    // cv::imshow("dst2", color_dst2);
   }
 
   return 0;
