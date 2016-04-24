bd34a508d41a5386165c4689793cc28785f5e6fb
Felix Schmoll <eggi.innovations@gmail.com>
2016-04-24 21:06:04 +0000

    Correct topic configuration in crash_recovery (#49)

--- team_3/crash_recovery/src/crash_recovery_node.cpp
+++ team_3/crash_recovery/src/crash_recovery_node.cpp
@@ -1,55 +1,45 @@
+/** @file crash_recovery_node.cpp
+ *  @brief Main executable of crash_recovery_noe
+ *
+ *  @author Leonhard Kuboschek (kuboschek)
+ *  @author Felix Schmoll (LiftnLearn)
+ */
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Pose2D.h>
 #include "../include/CrashRecoverer.h"
 
-/** @brief Helper function for retrieving float environment variables declared
-   * in the launch file. */
-void getEnvironmentVariable(const char *varString, std::string &var) {
-  char *c;
-
-  c = std::getenv(varString);
-  if (!c) {
-    ROS_INFO("Environment variable %s not found. Using %s as a default value.",
-             varString, var.c_str());
-  } else {
-    var = std::string(c);
-  }
-}
 
 /** Main executable for crash resolution advice node.
  *
  *  Subscribes to ```base_scan``` for laser data.
  *  Publishes to ```crash_recovery``` for publishing resolution advice. */
 int main(int argc, char **argv) {
-  std::string laser_topic = "base_scan";
-  std::string crash_topic = "crash_recovery";
+  ROS_ASSERT(argc > 4);
 
-  getEnvironmentVariable("laser_topic", laser_topic);
-  getEnvironmentVariable("crash_topic", crash_topic);
+  std::string laser_topic = argv[1];
+  std::string crash_topic = argv[2];
 
   ros::init(argc, argv, crash_topic);
 
   ros::NodeHandle n;
 
-  CrashRecoverer *recover = new CrashRecoverer();
+  CrashRecoverer recoverer;
 
   ros::Subscriber sub =
-      n.subscribe(laser_topic, 1, &CrashRecoverer::receiveLaserScan, recover);
+      n.subscribe(laser_topic, 1, &CrashRecoverer::receiveLaserScan, &recoverer);
 
   ros::Publisher pub = n.advertise< geometry_msgs::Twist >(crash_topic, 1);
 
   ros::Rate rate(10);
 
   while (ros::ok()) {
-
-    pub.publish(recover->getResolution());
+    pub.publish(recoverer.getResolution());
 
     ros::spinOnce();
     rate.sleep();
   }
-
-  delete recover;
 
   return 0;
 }
