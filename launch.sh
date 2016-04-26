451405cab8c12aabc6243beb34725561e93985cb
bklw <x32000@gmail.com>
2016-04-26 17:13:49 +0200

    Extended and updated the README

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@
 catkin_make
 catkin_make run_tests
 
-roslaunch lab_simulator simulator.launch &
+source ./devel/setup.bash
 
-rosrun half_circle_detection half_circle_detection_node &
-rosrun random_walk_strategy random_walk_strategy_node &
+roslaunch lab_simulator simulator.launch &
+roslaunch wall_following_strategy wallFollowing.launch
